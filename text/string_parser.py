0516ec65cb75c8d31d1ccd51752de45891ee6860
Yugeng Hui <interhuiyg@163.com>
2015-09-08 18:53:52 +0800

    Fix string_parser bugs

--- text/string_parser.py
+++ text/string_parser.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import types
+
 import regex_utils
 import string_utils
     
@@ -10,58 +12,54 @@ def parse(string, rule_list):
     
     string = string.strip()
     
-    for rule_item in rule_list:
-        rule_type = rule_item.get('type').strip() if rule_item.has_key('type') else ''
+    for rule in rule_list:
+        rule_type = rule.get('type').strip() if rule.has_key('type') else ''
         rule_type = 'regex' if rule_type == '' else rule_type
         
-        pattern = rule_item.get('pattern') if rule_item.has_key('pattern') else ''
+        pattern = rule.get('pattern') if rule.has_key('pattern') else ''
         
         if rule_type == 'regex':
             if regex_utils.check_line(pattern, string):
                 value_list = regex_utils.parse_line(pattern, string)
-                map_index = __build_map_index(rule_item)
+                map_index = __build_map_index(rule)
                 return __map_with_index(value_list, map_index)
         
         elif rule_type == 'split':
-            match = rule_item.get('match') if rule_item.has_key('match') else ''
+            match = rule.get('match') if rule.has_key('match') else ''
             if __is_match(match, string):
                 value_list = string.split(pattern)
-                map_index = __build_map_index(rule_item)
+                map_index = __build_map_index(rule)
                 return __map_with_index(value_list, map_index)
         
         elif rule_type == 'keyword':
-            match = rule_item.get('match') if rule_item.has_key('match') else ''
+            match = rule.get('match') if rule.has_key('match') else ''
             if __is_match(match, string):
-                map_index = __build_map_index(rule_item)
+                map_index = __build_map_index(rule)
                 return __map_with_keyword(string, map_index.values())
     
     return None
 
-def __build_map_index(rule_item):
-    map_index = {}
-    
-    map_index_list = rule_item.get('item') if rule_item.has_key('item') else []
-        
-    for _map_index in map_index_list:
-        try:
-            if _map_index.has_key('index') and _map_index.has_key('key'):
-                map_index[_map_index.get('index')] = _map_index.get('key')
-        except:
-            pass
-    
-    return map_index
+def __build_map_index(rule):
+    index_map = rule.get('map') if rule.has_key('map') else {}
+    return index_map
 
 def __is_match(match, line):
     result = False
     
-    if regex_utils.check_line('(\S+)\((\S+)\)', match):
-        fun, value = regex_utils.parse_line('(\S+)\((\S+)\)', match)
-        if fun == 'startswith':
-            result = line.startswith(value)
-        elif fun == 'endswith':
-            result = line.endswith(value)
-        elif fun == 'in':
-            result = value in line
+    if type(match) == types.StringType:
+        if regex_utils.check_line('(\S+)\((\S+)\)', match):
+            fun, value = regex_utils.parse_line('(\S+)\((\S+)\)', match)
+            if fun == 'startswith':
+                result = line.startswith(value)
+            elif fun == 'endswith':
+                result = line.endswith(value)
+            elif fun == 'in':
+                result = value in line
+    elif type(match) == types.FunctionType:
+        try:
+            result = match(line)
+        except:
+            result = False
     
     return result
    
@@ -72,7 +70,7 @@ def __map_with_index(value_list, map_index):
     value_map = {}
     index = 1
     for value in value_list:
-        key = map_index.get(str(index))
+        key = map_index.get(index)
         if key != None:
             value_map[key] = value
         index = index + 1
@@ -105,7 +103,8 @@ def __map_with_keyword(string, keywords):
                 value_map[key] = value.strip()
             key, value = item, ''
         else:
-            value = value + separator + item
+            if string_utils.is_not_blank(value) and string_utils.is_not_blank(item):
+                value = value + separator + item
             
     # Add last item to resultset   
     if key != None and value != None:
--- text_test/string_parser_test.py
+++ text_test/string_parser_test.py
@@ -2,14 +2,78 @@
 
 import unittest
 from text import string_parser
+from text import string_utils
 
 class StringParserTest(unittest.TestCase):
     
     def test_parse_by_keyword(self):
-        string = 'name test-acl src-addr 192.168.10.0 255.255.255.0 dst-addr host 192.168.199.3 service tcp 80 time-range NONE'
-        resultset = string_parser.parse(string, ['name', 'src-addr', 'dst-addr', 'service', 'time-range'])
+        
+        def match_line(line):
+            if string_utils.is_not_empty(line):
+                return len(string_utils.split(line, ';')) == 5
+            return False
+        
+        rule_list = [
+                {
+                    'type' : 'regex',
+                    'pattern' : 'WAF:name=(\S+); type=(\S+); protocol=(\S+); action=(\S+); message=(.*)',
+                    'map' : {
+                             1 : 'name',
+                             2 : 'type',
+                             3 : 'protocol',
+                             4 : 'action',
+                             5 : 'message'
+                    }
+                 },
+                {
+                    'type' : 'split',
+                    'pattern' : ';',
+                    'match' : match_line,
+                    'map' : {
+                             1 : 'date',
+                             2 : 'table',
+                             3 : 'category',
+                             4 : 'user',
+                             5 : 'ip'
+                    }
+                 },
+                {
+                    'type' : 'keyword',
+                    'match' : 'startswith(rule)',
+                    'map' : {
+                             1 : 'name',
+                             2 : 'src-addr',
+                             3 : 'dst-addr',
+                             4 : 'service',
+                             5 : 'time-range'
+                    }
+                 }
+            ]
+        
+        string = 'WAF:name=HTTP-CC; type=DDoS; protocol=tcp; action=deny; message=deny 59.61.33.121 HTTP-CC Attack'
+        resultset = string_parser.parse(string, rule_list)
+        self.assertEqual(resultset.get('name'), 'HTTP-CC')
+        self.assertEqual(resultset.get('type'), 'DDoS')
+        self.assertEqual(resultset.get('protocol'), 'tcp')
+        self.assertEqual(resultset.get('action'), 'deny')
+        self.assertEqual(resultset.get('message'), 'deny 59.61.33.121 HTTP-CC Attack')
+        
+        string = '2015-08-06;IB_BANK_TASK;Information;mofuxi;192.168.23.32'
+        resultset = string_parser.parse(string, rule_list)
+        self.assertEqual(resultset.get('date'), '2015-08-06')
+        self.assertEqual(resultset.get('table'), 'IB_BANK_TASK')
+        self.assertEqual(resultset.get('category'), 'Information')
+        self.assertEqual(resultset.get('user'), 'mofuxi')
+        self.assertEqual(resultset.get('ip'), '192.168.23.32')        
+        
+        string = 'rule name test-acl src-addr 192.168.10.0 255.255.255.0 dst-addr host 192.168.199.3 service tcp 80 time-range NONE'
+        resultset = string_parser.parse(string, rule_list)
         self.assertEqual(resultset.get('name'), 'test-acl')
         self.assertEqual(resultset.get('src-addr'), '192.168.10.0 255.255.255.0')
         self.assertEqual(resultset.get('dst-addr'), 'host 192.168.199.3')
         self.assertEqual(resultset.get('service'), 'tcp 80')
         self.assertEqual(resultset.get('time-range'), 'NONE')
+        
+if __name__ == '__main__':
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
