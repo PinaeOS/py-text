f6d5933442a59ad8279c6b645177293ac5196335
Yugeng Hui <interhuiyg@163.com>
2015-09-21 20:09:38 +0800

    add size/join/split to text_file.py

--- text/text_file.py
+++ text/text_file.py
@@ -2,122 +2,110 @@
 
 import os
 import types
-import urllib
+import codecs
 from text import string_utils
+from text import regex_utils
 
-def read(target):
-    
-    if target == None:
+def read_file(filename, encoding = 'utf-8'):
+    if filename == None:
         return None
     
-    target_type = type(target)
     text = []
-    if target_type == types.ListType or target_type == types.FunctionType:
-        text = target
-    elif target_type == types.StringType:
+    try:   
+        text_file = codecs.open(filename, 'r', encoding)
+        text = [line.strip() for line in text_file.readlines() if line != '']
+        text_file.close()
+    except IOError:
+        text = None
 
-        if target.startswith('http'):
-            try:
-                web_url = urllib.urlopen(target)
-                text = [line.strip() for line in web_url.readlines() if line != '']
-                web_url.close()
-            except IOError:
-                text = None
-        else:
-            try:   
-                text_file = open(target, 'r')
-                text = [line.strip() for line in text_file.readlines() if line != '']
-                text_file.close()
-            except IOError:
-                text = None
-
-    return text
-
-
-def read_files(filename_list):
-    
-    text = []
-    
-    if type(filename_list) == types.ListType:
-        for filename in filename_list:
-            text.extend(read(filename))
-    
     return text
 
 def write_file(filename, text, encoding = 'utf-8'):
-    #TODO write file with encoding
+    
     try:
-        text_file = open(filename, 'w')
+        text_file = codecs.open(filename, 'w', encoding)
         
         if type(text) == types.ListType:
             for line in text:
-                text_file.write(line)
+                if string_utils.is_not_empty(line):
+                    text_file.write(line + '\n')
         elif type(text) == types.StringType:
-            text_file.write(text)
+            if string_utils.is_not_empty(text):
+                text_file.write(text + '\n')
             
         text_file.close()
     except IOError:
         pass
     
-def split(filename, size, path, ext_fn, encoding = 'utf-8'):
+def list_dir(dir_path, file_filter = None):
+    file_list = []
+    if string_utils.is_not_empty(dir_path):
+        if os.path.isdir(dir_path):
+            filename_list = os.listdir(dir_path)
+            for filename in filename_list:
+                if file_filter != None and isinstance(file_filter, FileFilter):
+                    if file_filter.filter(filename):
+                        file_list.append(os.path.join(dir_path, filename))
+                else:
+                    file_list.append(os.path.join(dir_path, filename))
+    else:
+        file_list = None
+        
+    return file_list
+    
+def split(filename, size, dir_path, ext_fn, encoding = 'utf-8'):
     
     name = os.path.basename(filename)
     if string_utils.is_empty(name):
         name = 'split'
     
-    with open(filename, 'r') as f:
+    with codecs.open(filename, 'r', encoding) as f:
         file_counter = 1
         text = []
         for line in f:
             text.append(line)
             if len(text) >= size:
-                write_file(path + os.sep + name + '_' + str(file_counter) + '.' + ext_fn, text, encoding)
+                write_file(dir_path + os.sep + name + '_' + str(file_counter) + '.' + ext_fn, text, encoding)
                 text = []
                 file_counter = file_counter + 1
         if len(text) > 0:
-            write_file(path + os.sep + name + '_' + str(file_counter) + '.' + ext_fn, text, encoding)
+            write_file(dir_path + os.sep + name + '_' + str(file_counter) + '.' + ext_fn, text, encoding)
     pass
 
-def join(filename, dir_path):
-    pass
+def join(filename, dir_path, file_filter = None, encoding = 'utf-8'):
+    file_list = list_dir(dir_path, file_filter)
+    
+    text = []
+    for file_path in file_list:
+        text.extend(read_file(file_path, encoding))
+        
+    write_file(filename, text, encoding)
 
-def read_property(filename, comment=False):
-    config = {}
-    try:
-        _file = open(filename , 'r')
-        comment = ''
-        for line in _file.readlines():
-            line = line.strip()
-            if line.startswith('#') == False and line != '':
-                key, value = line.split('=')
-                if comment :
-                    config[key] = {}
-                    config[key]['value'] = value.strip()
-                    if comment != '':
-                        config[key]['comment'] = comment
-                        comment = ''
-                else:
-                    config[key] = value.strip()
-            else:
-                comment = line[1:len(line)]
-        _file.close()
-    except IOError:
-        config = None
-    return config
-
-
-def write_property(filename, config):
-    try:
-        _file = open(filename, 'w')
-        for key in config.keys():
-            _config_item = config.get(key)
-            if isinstance(_config_item, dict):
-                if _config_item.has_key('comment'):
-                    _file.write('#{0}\n'.format(_config_item.get('comment')))
-                if _config_item.has_key('value'):
-                    _file.write('{0}={1}\n'.format(key, _config_item.get('value')))
-            else:
-                _file.write('{0}={1}\n'.format(key, _config_item))
-        _file.close()
-    except IOError:
-        pass
+def size(filename):
+    if string_utils.is_empty(filename):
+        return 0
+    
+    size = 0
+    with codecs.open(filename, 'r') as f:
+        for _ in f:
+            size = size + 1
+    return size
+            
+class FileFilter(object):
+    def __init__(self, file_filter):
+        if file_filter != None and type(file_filter) == types.ListType:
+            self.file_filter = file_filter
+    
+    def filter(self, filename):
+        if string_utils.is_empty(filename):
+            return False
+        
+        if hasattr(self, "file_filter"):
+            flag = False
+            for filter_item in self.file_filter:
+                if regex_utils.check_line(str(filter_item), filename):
+                    flag = True
+                    break
+            return flag
+        else:
+            return True
